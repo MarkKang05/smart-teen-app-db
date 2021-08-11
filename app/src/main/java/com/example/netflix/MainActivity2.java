2ba2d4e5a6ceba973ee3a187407b1638090d71b5
MarkKang05 <markkang05@gmail.com>
2021-08-12 00:15:26 +0900

    Read firebase value & ListView

--- app/src/main/java/com/example/netflix/MainActivity2.java
+++ app/src/main/java/com/example/netflix/MainActivity2.java
@@ -1,16 +1,96 @@
 package com.example.netflix;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    ListView myListView;
+
+    ArrayList<String> myArrayList = new ArrayList<>();
+
+    DatabaseReference mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(MainActivity2.this, android.R.layout.simple_list_item_1, myArrayList);
+        myListView = (ListView) findViewById(R.id.listView);
+        myListView.setAdapter(myArrayAdapter);
+
+        mRef = FirebaseDatabase.getInstance().getReference();
+
+        mRef.child("todos").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot fileSnapshot : dataSnapshot.getChildren()) {
+                    //MyFiles filename = (MyFiles) fileSnapshot.getValue(MyFiles.class);
+                    System.out.print("chiid: "+fileSnapshot.child("id").getValue(int.class));
+                    System.out.println("child: "+fileSnapshot.child("content").getValue(String.class)+"========");
+                    String str = fileSnapshot.child("content").getValue(String.class) + " " + fileSnapshot.child("id").getValue(int.class);
+                    //System.out.println(" ======== ori"+fileSnapshot);
+                    myArrayList.add(str);
+                }
+                myArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+//        mRef.addChildEventListener(new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+//                String value = dataSnapshot.getValue(String.class);
+//                myArrayList.add(value);
+//                myArrayAdapter.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+//
+//                myArrayAdapter.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+//
+//            }
+//
+//            @Override
+//            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+
+
+
     }
 
     public void onClick_sc1(View v){
